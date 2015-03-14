a686874880c47b3f995d643947e853051eedab4c
Makis Tracend <makis.tracend@gmail.com>
2015-03-14 05:58:42 -0700

    :zap: populating request/response in middleware

--- app/helpers/express.js
+++ app/helpers/express.js
@@ -5,11 +5,20 @@ helper = Parent.extend({
 
 	admin: function( req, res, next ){
 		// check if there's admin data in the session
-		if( !req.session ) return next();
-		if( !req.session.admin ) return next();
-		// mirror admin data
-		req.admin = req.admin || {};
-		req.admin = _.extend( req.admin, req.session.admin );
+		if( !req.session || !req.session.admin ){
+			// nothing to do...
+		} else {
+			// mirror admin data
+			req.admin = req.admin || {};
+			req.admin = _.extend( req.admin, req.session.admin );
+			// broadcast to the user object (for view logic)
+			res.locals.user = res.locals.user || {};
+			res.locals.user.admin = true;
+		}
+		// add methods
+		req.isAuthenticated = req.isAuthenticated || function(){
+			return ( req.admin && !_.isEmpty( req.admin ) );
+		};
 		// continue...
 		next();
 	}
